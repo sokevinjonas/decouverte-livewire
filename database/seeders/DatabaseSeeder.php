3092f1a0a49f231506550e7fb19a1bd037629b2d
jonasdevpro <kjonasdevpro@gmail.com>
2024-04-27 04:20:50 +0200

    factory + seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Country;
+use App\Models\Product;
+use App\Models\Continent;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $continents = [
+            ['id' => 1, 'name' => 'Europe',],
+            ['id' => 2, 'name' => 'Asia',],
+            ['id' => 3, 'name' => 'Africa',],
+            ['id' => 4, 'name' => 'South America',],
+            ['id' => 5, 'name' => 'North America',],
+        ];
+        foreach ($continents as $continent) {
+            Continent::factory()->create($continent)
+                ->each(function ($c) {
+                    $c->countries()->saveMany(Country::factory(10)->make());
+                });;
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Product::factory(100)->create();
     }
 }
