ca3c467694e3e4c6174e9f1dd45c845a7a818e89
jonasdevpro <kjonasdevpro@gmail.com>
2024-04-27 04:23:52 +0200

    correction dans migration countries

--- database/migrations/2024_04_27_020302_create_countries_table.php
+++ database/migrations/2024_04_27_020302_create_countries_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 25);
+            $table->string('name');
             $table->foreignId('continent_id')->references('id')->on('continents');
             $table->timestamps();
         });
