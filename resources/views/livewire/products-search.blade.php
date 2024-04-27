fec40c17d90e92e2b40fdf490c40fd27aeb5c985
jonasdevpro <kjonasdevpro@gmail.com>
2024-04-27 04:55:46 +0200

    maj text

--- resources/views/livewire/products-search.blade.php
+++ resources/views/livewire/products-search.blade.php
@@ -1,14 +1,14 @@
 <div class="container mx-auto py-16 px-8">
     <div class="mb-4">
-        <input type="text" wire:model.live="search" placeholder="Search for Products">
+        <input type="text" wire:model.live="search" placeholder="Rechercher des produits">
     </div>
     <table class="table-auto w-full">
         <thead>
             <tr>
                 <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">ID</th>
                 <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Image</th>
-                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Title</th>
-                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Price</th>
+                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Titre</th>
+                <th class="py-2 px-3 bg-gray-100 border-b-2 text-left">Prix</th>
             </tr>
         </thead>
         <tbody>
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,14 +1,14 @@
 <div class="flex flex-col w-[300px] mx-auto py-16">
     <div class="flex gap-4 justify-between">
-        <input type="text" wire:model="todoText" wire:keydown.enter="addTodo" placeholder="Type and hit enter"
+        <input type="text" wire:model="todoText" wire:keydown.enter="addTodo" placeholder="Ecrivez ... et taper "
             class="flex-1">
-        <button class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white" wire:click="addTodo">Add
+        <button class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white" wire:click="addTodo">Ajouter
         </button>
     </div>
 
     <div class="py-6">
         @if (count($todos) == 0)
-            <p class="text-gray-500 text-center">There are no todos</p>
+            <p class="text-gray-500 text-center">Il n'y a pas de tâches à faire</p>
         @endif
         @foreach ($todos as $index => $todo)
             <div class="flex gap-4 justify-between items-center py-1">
